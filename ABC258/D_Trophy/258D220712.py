11b47fe69e6a2d92ff15b1432b8260b64d1e6460
Lee-AHCL <103102205+Lee-AHCL@users.noreply.github.com>
2022-07-12 19:41:07 +0900

    Update 258D220712.py

--- ABC258/D_Trophy/258D220712.py
+++ ABC258/D_Trophy/258D220712.py
@@ -1,10 +1,28 @@
-n, x = map(int, input().split())
-res = 100000000000000000000
-min_b = 10000000000
-s = 0
-for i in range(1, n + 1):
-    a, b = map(int, input().split())
-    s += a + b
-    min_b = min(b, min_b)
-    res = min(res, s + min_b * max(0, x - i))
-print(res)
+import math 
+ 
+def main():
+    n, x = map(int, input().split())
+    sm = 0
+    jeux = [list(map(int, input().split())) for _ in range(n)]
+    ans = math.inf 
+    
+    for i in range(n):
+        game = jeux[i]
+        x -= 1
+    
+        if x == 0:
+            break 
+        
+        sm += sum(game)
+        ans = min(ans, sm + game[1] * x)
+        
+            
+  
+    print(ans)
+    return 
+            
+        
+ 
+ 
+if __name__ == "__main__":
+    main()
