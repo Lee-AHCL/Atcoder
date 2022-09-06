d6de9ec68678006950a0bf7416ecf805f4508553
Lee-AHCL <103102205+Lee-AHCL@users.noreply.github.com>
2022-09-06 21:25:06 +0900

    Create 267B_220906.py

---
+++ ABC267/B_Split?/267B_220906.py
@@ -0,0 +1,35 @@
+
+def main():
+    s = list(input())
+    colstat = [
+        s[6] == "1",
+        s[3] == "1", 
+        s[7] == "1" or s[1] == "1",
+        s[4] == "1" or s[0] == "1", 
+        s[8] == "1" or s[2] == "1", 
+        s[5] == "1", 
+        s[9] == "1"
+    ]
+
+    # Checkpoint
+    
+    if s[0] == "1": 
+        print("No")
+        return 
+    
+    for i in range(7):
+        for j in range(i):
+            if colstat[i] and colstat[j]:
+                for k in range(j+1, i):
+                    if not colstat[k]:
+                        print("Yes")
+                        return 
+
+    print("No")
+    return
+
+
+if __name__ == "__main__":
+    main()
+    
+    
